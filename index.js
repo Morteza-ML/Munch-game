c34b3a90c4db774991485a0c7bec811829370b15
Morteza-ML <128712521+Morteza-ML@users.noreply.github.com>
2024-01-19 11:51:52 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-var a = Math.floor((Math.random()*6 +1));
-var b = Math.floor((Math.random()*6 +1));
-var plear1 = '.tts';
-var plear2 = '.bbs';
-function tas(adad,plear){
-    for (i=0; i<(6-adad) ;i++){
-
-        if (adad===3){
-            document.querySelector( plear +((i+1)*2)).style.display='None';
-        }
-
-        else if ( adad===1 || adad===2 ){
-            document.querySelector( plear +(7-(i+1))).style.display='None';
-        }
-        else{
-            document.querySelector( plear +(i+1)).style.display='None';
-        }
+function zakhere(){
+    var ll =document.getElementById('nam1').value;
+    var dd =document.getElementById('nam2').value;
+    if (ll.length > 10||dd>10){
+        alert('The number of characters must be less than 10!');
     }
-}
-tas(a,plear1);
-tas(b,plear2);
-if (a > b){
-    document.getElementsByClassName('imgg')[0].setAttribute('src','./chap.jpg');
-    document.getElementsByClassName('imgg')[1].setAttribute('src','./chap.jpg');
-}
-else if(a<b){
-    document.getElementsByClassName('imgg')[0].setAttribute('src','./rast.jpg');
-    document.getElementsByClassName('imgg')[1].setAttribute('src','./rast.jpg');
+    else if (ll===''||dd===''){
+        alert('It should not be empty!')
+    }
+    else if (ll===dd){
+        alert('The names must not be the same');
+    }
+
+    else{
+        let consoldita=[]
+        consoldita.push(ll);
+        consoldita.push(dd);
+        document.cookie= consoldita;
+        window.location.href='./index.html';
+    }
 }
